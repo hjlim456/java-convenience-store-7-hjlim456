af448c57dfcc9b08918426e5494f36c455db2984
hyojun <hjlim45678@gmail.com>
2024-11-06 13:05:23 +0900

    feat: Product의 "재고"와 "이벤트명" 확인 하는 기능 구현
    출력예시에 맞도록 재고가 0개 일때 "재고 없음", 이벤트명이 "none"일때 빈값으로 출력되도록 변경하였습니다.

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,17 +1,22 @@
 package store.view;
 
+import java.util.stream.Stream;
 import store.domain.Inventory;
 import store.domain.Product;
 
 public class OutputView {
+
     public static void printInventory(Inventory inventory) {
         System.out.println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n");
         inventory.getProducts()
                 .forEach(OutputView::printProduct);
     }
-
     private static void printProduct(Product product) {
-        System.out.printf("- %s %d원 %s개 %s%n",
-                product.getName(), product.getPrice(), product.getQuantity(), product.getPromotionName());
+        String quantityInfo = product.getQuantityInfo(product.getQuantity());
+        String promotionNameInfo = product.getPromotionNameInfo(product.getPromotionName());
+
+        System.out.printf("- %s %d원 %s %s%n",
+                product.getName(), product.getPrice(), quantityInfo, promotionNameInfo);
     }
+
 }
