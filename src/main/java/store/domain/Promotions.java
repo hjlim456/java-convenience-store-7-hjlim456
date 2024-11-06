a7309c9f60869b28974d813620105bc9f31effab
hyojun <hjlim45678@gmail.com>
2024-11-06 11:49:52 +0900

    feat: Promotions 일급 컬렉션 구현 및 파일 기반 Promotion 초기화 기능 추가
    읽어온 Promotion들을 상수처럼 불러올수 있도록 Promotion의 일급 컬렉션Promotions를 생성하였습니다.

---
+++ src/main/java/store/domain/Promotions.java
@@ -0,0 +1,24 @@
+package store.domain;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import store.Util.FileLoader;
+
+public class Promotions {
+    private static final List<Promotion> promotions = new ArrayList<>();
+
+    public Promotions(String filePath) {
+        initializeFromFile(filePath);
+    }
+    private void initializeFromFile(String filePath) {
+        try {
+            FileLoader.loadByFilePath(filePath).stream()
+                    .skip(1)
+                    .map(Promotion::createPromotion)
+                    .forEach(promotions::add);
+        } catch (IOException e) {
+            System.out.println("[ERROR] 파일을 읽는 도중 오류가 발생했습니다.");
+        }
+    }
+}
