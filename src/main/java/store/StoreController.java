1e4e2d13ee58cdfbda8d1773f6b7b9f60222d1f2
hyojun <hjlim45678@gmail.com>
2024-11-07 14:05:09 +0900

    feat: "특정 형식으로 입력된 문자열 파싱하는 기능" 구현

--- src/main/java/store/StoreController.java
+++ src/main/java/store/StoreController.java
@@ -1,7 +1,11 @@
 package store;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import store.domain.Inventory;
+import store.domain.Product;
 import store.domain.Promotions;
 import store.view.OutputView;
 
@@ -13,5 +17,25 @@ public class StoreController {
         Inventory inventory = new Inventory(PRODUCTS_FILE_ADDRESS);
         OutputView.printInventory(inventory);
 
+        System.out.println("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        String orderString = Console.readLine();
+        Map<String, Integer> order = parseOrderString(orderString);
+        System.out.println(order.get("콜라"));
+
+    }
+
+    private static Map<String, Integer> parseOrderString(String orderString) {
+        Map<String, Integer> orderRepository = new HashMap<>();
+
+        for (String item : orderString.split(",")) {
+            item = item.replaceAll("[\\[\\]]", "");
+            String[] parts = item.split("-");
+
+            String productName = parts[0];
+            int quantity = Integer.parseInt(parts[1]);
+
+            orderRepository.put(productName, quantity);
+        }
+        return orderRepository;
     }
 }
