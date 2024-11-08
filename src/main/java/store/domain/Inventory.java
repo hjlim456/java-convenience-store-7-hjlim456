2c5e470cefa9d049d7278eb49b9d987d7eb1c243
hyojun <hjlim45678@gmail.com>
2024-11-09 08:38:31 +0900

    feat: "이름으로 상품을 검색하는 기능" 구현

--- src/main/java/store/domain/Inventory.java
+++ src/main/java/store/domain/Inventory.java
@@ -2,47 +2,44 @@ package store.domain;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import store.Util.FileLoader;
+import store.domain.discount.Promotion;
 
 public class Inventory {
     private final static String LINE_SPLIT_SEPARATOR = ",";
 
     private final List<Product> products = new ArrayList<>();
 
-    public List<Product> getProducts() {
-        return List.copyOf(products);
-    }
 
     public Inventory(String filePath) {
         initializeFromFile(filePath);
     }
 
+    public List<Product> getProducts() {
+        return products;
+    }
     private void initializeFromFile(String filePath) {
         try {
             FileLoader.loadByFilePath(filePath).stream()
                     .skip(1)
                     .map(Product::create)
-                    .forEach(products::add);
+                    .forEach(product -> products.add(product));
         } catch (IOException e) {
             System.out.println("[ERROR] 파일을 읽는 도중 오류가 발생했습니다.");
         }
     }
-    public Product getProductByName(String productName) {
-        return products.stream()
-                .filter(product -> product.getName().equals(productName))//행사상품인지 그냥상품인지 위에거 그냥주는 문제점잇음..
-                .findFirst()
-                .orElseGet(() -> {
-                    throw  new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
-                });
-    }
-    public void sellProduct(String productName, int count) {
-        //프로모션 기간 확인후 프로모션기간이라면 프로모션제품에서 decreaseQuantity().
-        //프로모션제품의 수량이 부족한경우 기본제품에서 decreaseQuantity()
-        //영수증에서  getSoldInfomation()을 요청하면
 
-        Product product = getProductByName(productName);
-        product.decreaseQuantity(count);
-//        Receipt.addSoldInformation(product, count);
+    public List<Product> findByName(String productName) {
+        List<Product> matchedProducts = products.stream()
+                .filter(product -> product.getName().equals(productName))
+                .toList();
+        if (matchedProducts.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+        }
+        return matchedProducts;
     }
+
 }
--- src/test/java/store/domain/InventoryTest.java
+++ src/test/java/store/domain/InventoryTest.java
@@ -1,9 +1,11 @@
 package store.domain;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import store.domain.discount.Promotions;
 
 class InventoryTest {
     private static final String PROMOTIONS_FILE_ADDRESS = "src/main/resources/promotions.md";
@@ -19,39 +21,45 @@ class InventoryTest {
 
     }
 
-    @DisplayName("상품 구매시 Inventory의 재고가 차감됨을 확인한다.")
-    @Test
-    void 상품_구매시_재고_차감_테스트() {
-        inventory.sellProduct("콜라", 1);
-        Assertions.assertThat(inventory.getProductByName("콜라").getQuantity()).isEqualTo(9);
-    }
+//    @DisplayName("프로모션 기간중 상품 구매시  프로모션재고가 차감됨을 확인한다.")
+//    @Test
+//    void 프로모션_기간중_상품_구매시_프로모션재고가_차감_테스트() {
+//        inventory.sellProduct("콜라", 1);
+//        Assertions.assertThat(inventory.getProductByName("콜라").getQuantity()).isEqualTo(9);
+//    }
 
-    @DisplayName("상품 구매수량이 재고를 초과한 경우 ")
-    @Test
-    void 구매수량이_재고를_초과시_예외_테스트() {
+//    @DisplayName("프로모션 기간이 아닐때 상품 구매시 일반재고가 차감됨을 확인한다.")
+//    @Test
+//    void 프로모션_기간이_아닐떄_상품_구매시_일반재고가_차감_테스트() {
+//        inventory.sellProduct("콜라", 1);
+//        Assertions.assertThat(inventory.getProductByName("콜라").getQuantity()).isEqualTo(9);
+//    }
 
-        Assertions.assertThatThrownBy(() -> inventory.sellProduct("콜라", 100))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
-    }
+//    @DisplayName("상품 구매수량이 재고를 초과한 경우 ")
+//    @Test
+//    void 구매수량이_재고를_초과시_예외_테스트() {
+//
+//        Assertions.assertThatThrownBy(() -> inventory.sellProduct("콜라", 100))
+//                .isInstanceOf(IllegalArgumentException.class)
+//                .hasMessage("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+//    }
 
     @DisplayName(" 상품 이름으로 Product객체 찾는 기능 확인")
     @Test
-    void 존재_하는_상품_입력시_Product객체_반환_테스트() {
+    void 존재_하는_상품_입력시_해당_Product_객체_반환_테스트() {
         String ExistsName = "콜라";
-        Product product = inventory.getProductByName(ExistsName);
+        List<Product> matchProduct = inventory.findByName(ExistsName);
 
-        Assertions.assertThat(product.getName()).isEqualTo(ExistsName);
-    }
+        Assertions.assertThat(matchProduct)
+                .allMatch(product -> product.getName().equals(ExistsName));    }
 
     @DisplayName("존재하지않는 상품 입력시 예외 처리 테스트")
     @Test
     void 존재_하지않는_상품_입력시_예외_테스트() {
         String notExistsName = "샤넬가방";
 
-        Assertions.assertThatThrownBy(() -> inventory.getProductByName(notExistsName))
+        Assertions.assertThatThrownBy(() -> inventory.findByName(notExistsName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
     }
-
 }
