8fa4931c83c26bbbb17b2562dbe895056d8c9e06
hyojun <hjlim45678@gmail.com>
2024-11-11 23:49:02 +0900

    fix: 뭐라도 변경

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -3,12 +3,10 @@ package store;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws IOException {
+
             StoreController storeController = new StoreController();
             storeController.run();
-        } catch (IOException e) {
-            System.out.println("[ERROR] 파일을 읽는 도중 오류가 발생했습니다.");
-        }
+
     }
 }
