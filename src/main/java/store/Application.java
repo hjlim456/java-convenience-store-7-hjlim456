25eb5fde6861699bb5629a43c9ec767029035f3a
hyojun <hjlim45678@gmail.com>
2024-11-11 23:51:55 +0900

    fix: 예외 수정

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -9,6 +9,7 @@ public class Application {
             storeController.run();
         } catch (IOException e) {
             System.out.println("[ERROR] 파일을 읽는 도중 오류가 발생했습니다.");
+            throw new IllegalArgumentException("[ERROR] 파일을 읽는 도중 오류가 발생했습니다.");
         }
     }
 }
